337c59a71f1e15411f61a9a20092327476bf4b6f
Kuber Singh <kuber301193@gmail.com>
2023-10-29 17:27:41 -0400

    commit

--- app/src/main/java/com/example/finalprojecttaskmanagementapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/finalprojecttaskmanagementapp/SplashScreenActivity.java
@@ -10,5 +10,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/finalprojecttaskmanagementapp/UserUtility.java
@@ -0,0 +1,4 @@
+package com.example.finalprojecttaskmanagementapp;
+
+public class UserUtility {
+}
