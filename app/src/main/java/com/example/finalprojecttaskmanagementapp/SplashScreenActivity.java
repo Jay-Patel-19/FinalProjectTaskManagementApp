88a47f59d6fe71c5510fc03bd2876f189f00d7f4
Harshilgit <astro.harshil@gmail.com>
2023-10-29 18:28:50 -0400

    harshil

--- app/src/main/java/com/example/finalprojecttaskmanagementapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/finalprojecttaskmanagementapp/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.finalprojecttaskmanagementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // Define the delay in milliseconds
+        int splashScreenDuration = 3000; // 3 seconds
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity
+                Intent mainIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(mainIntent);
+                finish(); // Close the splash screen activity
+            }
+        }, splashScreenDuration);
     }
 }
