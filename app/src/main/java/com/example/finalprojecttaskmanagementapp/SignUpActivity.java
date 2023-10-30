258189b8b1756c76cead58d3c156e6df16bb3c83
Kuber Singh <kuber301193@gmail.com>
2023-10-29 20:29:35 -0400

    commit

--- app/src/main/java/com/example/finalprojecttaskmanagementapp/SignUpActivity.java
+++ app/src/main/java/com/example/finalprojecttaskmanagementapp/SignUpActivity.java
@@ -3,12 +3,48 @@ package com.example.finalprojecttaskmanagementapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText txtFirstname, txtLastname, txtEmail, txtPassword;
+    Button gotoLogin, registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        UserUtility userutil = new UserUtility();
+        txtFirstname = findViewById(R.id.txtFirstname);
+        txtLastname = findViewById(R.id.txtLastname);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+        gotoLogin = findViewById(R.id.gotoLogin);
+        registerBtn = findViewById(R.id.registerBtn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstname = txtFirstname.getText().toString().trim();
+                String lastname = txtLastname.getText().toString().trim();
+                String email = txtEmail.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+                boolean signup = userutil.signUp(firstname, lastname, email, password);
+
+                if (signup){
+                    Toast.makeText(SignUpActivity.this, "hua na bc", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(SignUpActivity.this, "ma chuda", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
+
     }
 }
