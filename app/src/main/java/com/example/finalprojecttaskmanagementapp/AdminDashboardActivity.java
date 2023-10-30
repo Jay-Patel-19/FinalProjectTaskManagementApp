20c7efbc94630792d1e937f930989d12d60b13a2
Harshilgit <astro.harshil@gmail.com>
2023-10-29 22:52:43 -0400

    harshil

--- app/src/main/java/com/example/finalprojecttaskmanagementapp/AdminDashboardActivity.java
+++ app/src/main/java/com/example/finalprojecttaskmanagementapp/AdminDashboardActivity.java
@@ -10,5 +10,17 @@ public class AdminDashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+
+        // Create an array of UserItem objects with your data
+        ArrayList<UserItem> userItems = new ArrayList<>();
+        userItems.add(new UserItem("John", "Doe", "john.doe@example.com"));
+        userItems.add(new UserItem("Jane", "Smith", "jane.smith@example.com"));
+        // Add more UserItem objects as needed
+
+        // Create a custom adapter and set it for the ListView
+        CustomUserItemAdapter adapter = new CustomUserItemAdapter(this, userItems);
+
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
     }
 }
